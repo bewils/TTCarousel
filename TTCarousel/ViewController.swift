35dad75349943cb880e8177bf2eb0ee0f88a5175
李锐 <correctforever@gmail.com>
2017-04-05 21:33:55 +0800

    Initial Commit

---
+++ TTCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTCarousel
+//
+//  Created by 李锐 on 2017/4/5.
+//  Copyright © 2017年 李锐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
